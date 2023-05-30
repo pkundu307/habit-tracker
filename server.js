7877dbeab1fa2aca904c769df3e1e8ed93ad1f86
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2023-05-31 00:39:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //import express module
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT||3000;
 
 const app = express();
 const cookieParser = require('cookie-parser');
